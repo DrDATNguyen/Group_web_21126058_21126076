b8e2c7a424ca0e78647b84805e2ed7c7752f63a9
DrDATNguyen <102152763+DrDATNguyen@users.noreply.github.com>
2025-01-07 08:14:01 +0700

    update

--- templatemo_571_hexashop/routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { apiGetProducts } = require('../controllers/productController');
-
-router.get('/', apiGetProducts); // Hiển thị trang đăng nhập
-
-module.exports = router;
